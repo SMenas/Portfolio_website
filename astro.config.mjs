abd0d7ad621d0c7d3495326f3dea4f841f7b913f
Senna Embsen <senna.embsen@gmail.com>
2024-10-17 12:56:53 +0200

    img veranderingen + dev toolbar false

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
+  devToolbar: {
+    enabled: false},
    site: 'https://lexingtonthemes.com',
   integrations: [tailwind(),  sitemap()]
 });
