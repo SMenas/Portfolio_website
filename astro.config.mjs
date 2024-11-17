3ec87ea24759c99ebaa61a5a915c78b2cd7f482b
Senna Embsen <senna.embsen@gmail.com>
2024-11-17 20:08:19 +0100

    back to when it worked

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
 import netlify from '@astrojs/netlify/functions';  // This line was likely the issue
 
 export default defineConfig({
-  output: 'server',
+  devToolbar: {
+    enabled: false},
+  integrations: [tailwind(),  sitemap()],
+  output: "server",
   adapter: netlify()  // Make sure this matches the import above
 });
